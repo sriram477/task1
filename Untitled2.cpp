4777e25aad378cfd40185f136a5881653f717378
sriram477 <83854256+sriram477@users.noreply.github.com>
2021-05-08 10:29:35 +0530

    task 1
    this 2 file are task 1 answers

---
+++ Untitled2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	double firstnumber,secondnumber,productoftwonumbers;
+	cout<<"enter two numbers:";
+	cin>>firstnumber >>secondnumber;
+	productoftwonumbers=firstnumber*secondnumber;
+	cout<<"product="<<productoftwonumbers;
+	return 0;
+}
+
---
+++ Untitled3.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	clrscr();
+cout<<"size of int:"<<sizeof(int)<<"bytes"<<endl;
+cout<<"size of float:"<<sizeof(float)<<"bytes"<<endl;
+cout<<"size of double:"<<sizeof(double)<<"bytes"<<endl;
+cout<<"size of char:"<<sizeof(char)<<"byte"<<endl;
+return 0;
+}
